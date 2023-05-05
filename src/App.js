1e931679976080dd3515af83fbc632e6577e05ce
shebl <shebblloll@gmail.com>
2023-05-05 16:42:14 +0300

    sreach moives function ussing API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from "react";
+import { React, useEffect } from "react";
+// 4c1c6ebf  {api key}
 
+// API url + API key
+const API_URL = "http://www.omdbapi.com/?apikey=4c1c6ebf";
 const App = () => {
+  const sreachMoives = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  };
+  useEffect(() => {
+    sreachMoives("god");
+  }, []);
   return (
     <>
       <h1>hello</h1>
